cbb86bd0a42f91a187cabe73811f2c086e64e17e
lemor303442 <yuki.303442@gmail.com>
2017-09-17 19:35:57 +0900

    playerがちゃんと動けるかな？

--- Assets/Scripts/CharacterScripts/PlayerController.cs
+++ Assets/Scripts/CharacterScripts/PlayerController.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : Photon.MonoBehaviour
+{
 
 	public float goForwardSpeed = 5;
 	public float goSideSpeed = 3;
@@ -11,22 +12,34 @@ public class PlayerController : MonoBehaviour {
 	public float goBackRotation = 120;
 	public bool isPlayerDemon;
 
-	public void Update () {
-		// playerの動き
-		if(Input.GetKey ("up") == true) {
-			this.transform.position += this.transform.forward * goForwardSpeed * Time.deltaTime;
+	void Start() {
+		if (photonView.isMine) {
+			this.transform.Find("PlayerCamera").gameObject.SetActive(true);
 		}
-		if(Input.GetKey ("down") == true) {
-			this.transform.position -= this.transform.forward * goForwardSpeed * Time.deltaTime;
-			this.transform.Rotate (new Vector3 (0, goBackRotation * Time.deltaTime, 0));
-		}
-		if(Input.GetKey ("right") == true) {
-			this.transform.position += this.transform.right * goSideSpeed * Time.deltaTime;
-			this.transform.Rotate (new Vector3 (0, goSideRotation * Time.deltaTime, 0));
-		}
-		if(Input.GetKey ("left") == true) {
-			this.transform.position -= this.transform.right * goSideSpeed * Time.deltaTime;
-			this.transform.Rotate (new Vector3 (0, -goSideRotation * Time.deltaTime, 0));
+	}
+
+	void Update ()
+	{
+		if (photonView.isMine) {
+			// playerの動き
+			if (Input.GetKey ("up") == true) {
+				this.transform.position += this.transform.forward * goForwardSpeed * Time.deltaTime;
+			}
+			if (Input.GetKey ("down") == true) {
+				this.transform.position -= this.transform.forward * goForwardSpeed * Time.deltaTime;
+				this.transform.Rotate (new Vector3 (0, goBackRotation * Time.deltaTime, 0));
+			}
+			if (Input.GetKey ("right") == true) {
+				this.transform.position += this.transform.right * goSideSpeed * Time.deltaTime;
+				this.transform.Rotate (new Vector3 (0, goSideRotation * Time.deltaTime, 0));
+			}
+			if (Input.GetKey ("left") == true) {
+				this.transform.position -= this.transform.right * goSideSpeed * Time.deltaTime;
+				this.transform.Rotate (new Vector3 (0, -goSideRotation * Time.deltaTime, 0));
+			}
+		}else{
+			
+
 		}
 	}
 }
