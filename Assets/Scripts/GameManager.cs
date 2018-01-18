29a28b7e4bc5222e81d8f02d6633b97e2aec7f62
gekidan_shiki <masumoto@sc0ville.com>
2018-01-19 02:23:23 +0900

    Create network-system

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Com.MyCompany.MyGame {
+  public class GameManager : Photon.PunBehaviour {
+
+    void LoadArena () {
+      if (!PhotonNetwork.isMasterClient) {
+        Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+      }
+      Debug.Log("PhotonNetwork : Loading Level : " + PhotonNetwork.room.playerCount);
+      // LoadLevel is should be called by master client.
+      PhotonNetwork.LoadLevel("Room for "+PhotonNetwork.room.playerCount);
+    }
+
+    public void OnLeftRoom () {
+      SceneManager.LoadScene(0);
+    }
+
+    public void LeaveRoom () {
+      PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnPhotonPlayerConnected( PhotonPlayer other ) {
+      Debug.Log("OnPhotonPlayerConnected() " + other.name);
+      if(PhotonNetwork.isMasterClient) {
+        Debug.Log("OnPhotonPlayerConnected isMasterClient " + PhotonNetwork.isMasterClient);
+        LoadArena();
+      }
+    }
+
+    public override void OnPhotonPlayerDisconnected( PhotonPlayer other ) {
+      Debug.Log("OnPhotonPlayerDisconnected() " + other.name);
+      if(PhotonNetwork.isMasterClient) {
+        Debug.Log("OnPhotonPlayerDisconnected isMasterClient " + PhotonNetwork.isMasterClient);
+        LoadArena();
+      }
+    }
+
+  }
+}
---
+++ Assets/Scripts/PlayerScript/playerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+  float goForwardSpeed = 5;
+  float turnRotation = 90;
+
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+	  Move();	
+	}
+
+  void Move () {
+    if (Input.GetKey("up")) {
+      this.transform.position += this.transform.forward * goForwardSpeed * Time.deltaTime;
+    }
+    if (Input.GetKey("down")) {
+      this.transform.position -= this.transform.forward * goForwardSpeed * Time.deltaTime;
+    }
+    if (Input.GetKey("right")) {
+      this.transform.Rotate(new Vector3 (0, turnRotation * Time.deltaTime, 0));
+    }
+    if (Input.GetKey("left")) {
+      this.transform.Rotate(new Vector3 (0, -turnRotation * Time.deltaTime, 0));
+    }
+  }
+
+}
